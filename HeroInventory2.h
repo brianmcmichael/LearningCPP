25e5d67e2714f25a139aafb6f8d14a5660668dda
brianmcmichael <brian@brianmcmichael.com>
2012-11-16 22:28:52 -0500

    HeroInventory2

---
+++ HeroInventory2.h
@@ -0,0 +1,17 @@
+/*
+ * HeroInventory2.h
+ *
+ *  Created on: Nov 16, 2012
+ *      Author: Brian
+ */
+
+#ifndef HEROINVENTORY2_H_
+#define HEROINVENTORY2_H_
+
+class HeroInventory2 {
+public:
+	HeroInventory2();
+	virtual ~HeroInventory2();
+};
+
+#endif /* HEROINVENTORY2_H_ */
