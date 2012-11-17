f11c812948408258a1a8a234c4f119ee80b17ba0
Brian McMichael <brian@brianmcmichael.com>
2012-11-16 22:15:44 -0500

    ScoreRater2.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 LearningCPP
 ===========
 
-Code backups for C++ projects I'm playing with.
+Code backups for C++ projects I'm playing with. Nothing original here, just stuff copied out of a book.
---
+++ ScoreRater2.h
@@ -0,0 +1,17 @@
+/*
+ * ScoreRater2.h
+ *
+ *  Created on: Nov 16, 2012
+ *      Author: Brian
+ */
+
+#ifndef SCORERATER2_H_
+#define SCORERATER2_H_
+
+class ScoreRater2 {
+public:
+	ScoreRater2();
+	virtual ~ScoreRater2();
+};
+
+#endif /* SCORERATER2_H_ */
