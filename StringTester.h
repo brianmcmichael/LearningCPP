c2956ced70061d0593c2ce8206d9d85208ae6084
brianmcmichael <brian@brianmcmichael.com>
2012-11-16 22:22:38 -0500

    StringTester

---
+++ StringTester.h
@@ -0,0 +1,17 @@
+/*
+ * StringTester.h
+ *
+ *  Created on: Nov 16, 2012
+ *      Author: Brian
+ */
+
+#ifndef STRINGTESTER_H_
+#define STRINGTESTER_H_
+
+class StringTester {
+public:
+	StringTester();
+	virtual ~StringTester();
+};
+
+#endif /* STRINGTESTER_H_ */
