bfe791e9dca8409a4dcf011cefca4e1627adc738
brianmcmichael <brian@brianmcmichael.com>
2012-11-17 15:27:03 -0500

    HeroInv3

---
+++ HeroInventory3.h
@@ -0,0 +1,20 @@
+/*
+ * HeroInventory3.h
+ *
+ *  Created on: Nov 17, 2012
+ *      Author: Brian
+ */
+
+#ifndef HEROINVENTORY3_H_
+#define HEROINVENTORY3_H_
+
+namespace std {
+
+class HeroInventory3 {
+public:
+	HeroInventory3();
+	virtual ~HeroInventory3();
+};
+
+} /* namespace std */
+#endif /* HEROINVENTORY3_H_ */
