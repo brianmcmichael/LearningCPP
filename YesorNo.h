d34a022a630cbdffdb4d420b1eacd674298cf71b
brianmcmichael <brian@brianmcmichael.com>
2012-11-21 20:53:23 -0800

    YesNo

---
+++ YesorNo.h
@@ -0,0 +1,17 @@
+/*
+ * YesorNo.h
+ *
+ *  Created on: Nov 21, 2012
+ *      Author: brian_000
+ */
+
+#ifndef YESORNO_H_
+#define YESORNO_H_
+
+class YesorNo {
+public:
+	YesorNo();
+	virtual ~YesorNo();
+};
+
+#endif /* YESORNO_H_ */
