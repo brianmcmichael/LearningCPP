e7c7dacdcae31be7c4ca72a1631b3666b2eb775d
brianmcmichael <brian@brianmcmichael.com>
2012-11-21 20:12:38 -0800

    Instructions

---
+++ OldProjects/Instructions.h
@@ -0,0 +1,17 @@
+/*
+ * Instructions.h
+ *
+ *  Created on: Nov 21, 2012
+ *      Author: brian_000
+ */
+
+#ifndef INSTRUCTIONS_H_
+#define INSTRUCTIONS_H_
+
+class Instructions {
+public:
+	Instructions();
+	virtual ~Instructions();
+};
+
+#endif /* INSTRUCTIONS_H_ */
