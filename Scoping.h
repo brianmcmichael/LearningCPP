5185aea45bec5bc4988a0d9fa52fb35accfa0d16
brianmcmichael <brian@brianmcmichael.com>
2012-11-22 18:48:34 -0800

    Scoping

---
+++ Scoping.h
@@ -0,0 +1,20 @@
+/*
+ * Scoping.h
+ *
+ *  Created on: Nov 22, 2012
+ *      Author: brian_000
+ */
+
+#ifndef SCOPING_H_
+#define SCOPING_H_
+
+namespace std {
+
+class Scoping {
+public:
+	Scoping();
+	virtual ~Scoping();
+};
+
+} /* namespace std */
+#endif /* SCOPING_H_ */
